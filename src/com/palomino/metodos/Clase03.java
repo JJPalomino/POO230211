6d8b0123a28371b7e49ee0dcec42ec472a1c91a1
JJPalomino <josepalomino_@outlook.com>
2023-02-11 17:51:35 -0600

    Cambio de método

--- src/com/palomino/metodos/Clase03.java
+++ src/com/palomino/metodos/Clase03.java
@@ -4,7 +4,7 @@ public class Clase03 {
     public static void celular(String marca, int precio, String modelo, int megapixeles, int bateria){
         System.out.println(
                 "El celular tiene las siguientes caracteristicass:"+ "\nLa marca es: " + marca
-                + "\nEl precio es: " + precio + "pesos" + "\nEl modelo es: " + modelo + "\nLos Megapixeles de la camara son: " +
+                + "\nEl precio es: " + precio + " "+ "pesos" + "\nEl modelo es: " + modelo + "\nLos Megapixeles de la camara son: " +
                  megapixeles + "\nLa bateria es de: " + bateria + "miliamperios"
         );
 
